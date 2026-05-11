ef053fff51d8541cae93bc62b73d75eb1da70a4f
anuragsoni5643 <sonianurag506@gmail.com>
2026-05-11 19:28:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 var isDate = function (input) {
-  //   write your code here
+ 
+    // Check if input is a valid Date object
+    if (input instanceof Date && !isNaN(input.getTime())) {
+        return true;
+    }
+
+    // Check DD/MM/YYYY format
+    const regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+
+    if (typeof input === "string" && regex.test(input)) {
+        return true;
+    }
+
+    return false;
 };
 
-// Do not change the code below.
+// Do not change the code below. 
 const input = prompt("Enter Date.");
 alert(isDate(input));
+
